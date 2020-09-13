f4b993eb168fab381265685d649a348b8d8a51b1
Jose Pivaral <josepiva15@gmail.com>
2020-09-12 18:19:57 -0600

    added sat class using xjc

--- src/main/java/com/pivaral/facturas/service/UploadService.java
+++ src/main/java/com/pivaral/facturas/service/UploadService.java
@@ -1,17 +1,17 @@
 package com.pivaral.facturas.service;
 
-import java.io.BufferedReader;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
+import java.util.logging.Logger;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.core.Response;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 
+import com.pivaral.facturas.model.sat.GTDocumento;
 import com.pivaral.facturas.utils.FileHelper;
 
 @RequestScoped
@@ -20,6 +20,8 @@ public class UploadService {
 	@Inject
 	private FileHelper fileHelper;
 	
+	private Logger logger = Logger.getLogger( UploadService.class.getName()); 
+	
 	public Response uploadXml(
 			InputStream uploadedInputStream,
 			FormDataContentDisposition fileDetails) {
@@ -27,11 +29,22 @@ public class UploadService {
 		var opExtension = fileHelper.getExtensionByStringHandling(fileDetails.getFileName());
 		if(opExtension.isPresent()) {
 			if(opExtension.get().equalsIgnoreCase("XML")) {
-				String text = new BufferedReader(
+				try {
+				    var jaxbContext = JAXBContext.newInstance(GTDocumento.class);            
+				    var jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+				    var document = (GTDocumento) jaxbUnmarshaller.unmarshal(uploadedInputStream);
+				    return Response.ok(document).build();
+				}
+				catch (JAXBException e) 
+				{
+					e.printStackTrace();
+					return Response.status(500).entity(e.getMessage() + " --- " + e.getLocalizedMessage()).build();
+				}				 
+				
+				/*String text = new BufferedReader(
 					      new InputStreamReader(uploadedInputStream, StandardCharsets.UTF_8))
 					        .lines()
-					        .collect(Collectors.joining("\n"));
-				return Response.ok(text).build();
+					        .collect(Collectors.joining("\n"));*/
 			} else {
 				return Response.status(400).build();	
 			}
